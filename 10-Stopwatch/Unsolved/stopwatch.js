488a5488c58630efccfc825e2dc9a787cda11689
Casey Carroll <caseydean8@gmail.com>
2020-01-16 11:35:21 -0500

    Stop button works

--- 10-Stopwatch/Unsolved/stopwatch.js
+++ 10-Stopwatch/Unsolved/stopwatch.js
@@ -17,7 +17,6 @@ var time = 0;
 var lap = 1;
 
 function reset() {
-
   time = 0;
   lap = 1;
 
@@ -26,34 +25,28 @@ function reset() {
 }
 
 function start() {
-
   //  TODO: Use setInterval to start the count here and set the clock to running.
   if (!clockRunning) {
-    time = setInterval(count, 1000);
-    clockRunning = true;
+    intervalId = setInterval(count, 1000);
   }
-
+  clockRunning = true;
 }
-function stop() {
 
+function stop() {
   //  TODO: Use clearInterval to stop the count here and set the clock to not be running.
-  clearInterval(time);
+  clearInterval(intervalId);
   clockRunning = false;
 }
 
 function recordLap() {
-
   //  TODO: Get the current time, pass that into the timeConverter function,
   //        and save the result in a variable.
-
   //  TODO: Add the current lap and time to the "laps" div.
-
   //  TODO: Increment lap by 1. Remember, we can't use "this" here.
 }
 function count() {
-
   //  TODO: increment time by 1, remember we cant use "this" here.
-  time++
+  time++;
   //  TODO: Get the current time, pass that into the timeConverter function,
   //        and save the result in a variable.
   let timeConverted = timeConverter(time);
@@ -65,10 +58,9 @@ function count() {
 //  We do not need to touch it.
 
 function timeConverter(t) {
-
   //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
   var minutes = Math.floor(t / 60);
-  var seconds = t - (minutes * 60);
+  var seconds = t - minutes * 60;
 
   if (seconds < 10) {
     seconds = "0" + seconds;
@@ -76,12 +68,9 @@ function timeConverter(t) {
 
   if (minutes === 0) {
     minutes = "00";
-  }
-
-  else if (minutes < 10) {
+  } else if (minutes < 10) {
     minutes = "0" + minutes;
   }
 
   return minutes + ":" + seconds;
 }
-
