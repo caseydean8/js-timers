189b5e976b398dbb27b4f0d5ac68d93f69f8506a
Casey Carroll <caseydean8@gmail.com>
2020-01-27 11:43:04 -0500

    Palindromes display in red

--- 22-PalindromeDetector/Unsolved/app.js
+++ 22-PalindromeDetector/Unsolved/app.js
@@ -16,22 +16,24 @@ const palCheck = string => {
   reverseArr = reverseArr.join("").trim();
   reverseArr = reverseArr.split(" ");
   let palSpan = document.createElement("span");
-  // palSpan.style.color = "red";
+  palSpan.style.color = "red";
+  console.log(palSpan);
 
   for (let i = 0; i < reverseArr.length; i++) {
     // console.log(reverseArr[i]);
     // console.log(stringArr[i]);
     if (reverseArr[i].toLowerCase() === stringArr[i].toLowerCase()) {
       console.log(reverseArr[i]);
-      reverseArr[i] = document.createElement(`<span style="color: 'red'">${reverseArr[i]}</span>`);
-      // reverseArr[]
-    console.log(reverseArr[i]);
+      // span = document.createElement(`<span style="color: red;">${reverseArr[i]}</span>`);
+      palSpan.textContent = reverseArr[i];
+      reverseArr[i] = palSpan.outerHTML;
+      console.log(reverseArr[i]);
+    }
   }
-}
   // reverseArr = reverseArr[0].split('');
   console.log(reverseArr);
   // console.log(rString);
-  document.getElementById("result").textContent = reverseArr;
+  document.getElementById("result").innerHTML = reverseArr;
 };
 
 palCheck("Your Mom");
