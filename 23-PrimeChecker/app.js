53f1403daade690ae52702a39fd1149c16af86b3
Casey Carroll <caseydean8@gmail.com>
2020-01-27 20:36:20 -0500

    Logs correct answer and possible divisors

--- 23-PrimeChecker/app.js
+++ 23-PrimeChecker/app.js
@@ -1,13 +1,17 @@
 const primeChecker = (num) => {
+  let isPrime = true;
+  let divisors = [];
+
   for (let i = 2; i < num-1; i++) {
-    console.log(num % i)
     if (num % i === 0) {
-      console.log(`${num} is not a prime number`)
-      break
-    } else {
-      console.log(`${num} is a prime number`)
+      isPrime = false
+      divisors.push(i);
     }
   }
+  isPrime ? console.log(`${num} is a prime number`) : console.log(`${num} is divisible by ${divisors}`)
 }
 
+primeChecker(12)
 primeChecker(13)
+primeChecker(132)
+primeChecker(1)
